c4ae2734d7f7c748cbec0bfd0ce225001346241d
Nivea-izeLu <niveapontes02@gmail.com>
2024-12-11 07:19:22 -0300

    Add files via upload
    páginas do site

---
+++ JavaScript.js
@@ -0,0 +1,17 @@
+    function ClickMenu() {
+       if (DPmenu.style.display == 'block') {
+        DPmenu.style.display = 'none'
+       } else{
+        DPmenu.style.display = 'block'
+       }
+    }
+    function ClickPerfil(){
+        if (DPperfil.style.display == 'block') {
+        DPperfil.style.display = 'none'
+       } else{
+        DPperfil.style.display = 'block'
+       }
+    }
+    function ClickDuvidas(){
+       window.alert("Enviado com sucesso!")
+    }
